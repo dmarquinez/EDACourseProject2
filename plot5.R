cbb05d172dac1311c0ea7b1b49d55a0a8e1be6b6
David Marquinez <d.marquinez@alumnos.upm.es>
2020-09-12 21:38:43 +0200

    Plot 5 done, error in plot 4 fixed

--- plot5.R
+++ plot5.R
@@ -13,18 +13,20 @@ SCC <- readRDS("Source_Classification_Code.rds")
 library(ggplot2)
 library(reshape2)
 library("dplyr")
-indexCoal <- grep("Coal",SCC$EI.Sector,ignore.case = TRUE)
-indexCoal2 <- grep("coal",SCC$Short.Name,ignore.case = TRUE)
-indexSCC <- SCC[union(indexCoal,indexCoal2),"SCC"]
-coal <- filter(NEI, NEI$SCC %in% indexSCC )
-p1 <- tapply(coal$Emissions, coal$year, sum)
-#Opening png
-png(filename = "plot4.png",width = 480, height = 480)
-#Creating plot
+index <- c()
+for(i in 1:length(names(SCC))){
+  index <- union(index,grep("vehicle",ignore.case = TRUE,SCC[,i]))
+}
+vehicle <- NEI[index,"SCC"]
+vehicleFilt <- filter(NEI, NEI$SCC %in% vehicle )
+p1 <- tapply(vehicleFilt$Emissions, vehicleFilt$year, sum)
 df <- data.frame(year=names(p1),Emissions=p1)
+#Opening png
+png(filename = "plot5.png",width = 480, height = 480)
+#Creating plot
 a <- ggplot(df,aes(year,Emissions,group=1))
 a+geom_line(size=2,col="brown")+ labs(title = "total PM2.5 emission in US",
-                                      subtitle = "from coal combustion-related sources",
+                                      subtitle = "from motor vehicle sources",
                                       y = "Emissions (in tons)",
                                       x = "Year")
 #Closing device
